d55274451a63b2b7f4978bf76d9bc7caf45207e1
Hasan-TechNinja <hasantechninja@gmail.com>
2025-08-10 10:08:31 +0600

    QR history & Notification admin registe

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile, EmailVerification
+from .models import UserProfile, EmailVerification, QRCodeHistory, Notification
 
 # Register your models here.
 
@@ -7,3 +7,15 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'bio', 'mobile_number', 'profile_picture')
     search_fields = ('user__username', 'user__email')
 admin.site.register(UserProfile, UserProfileAdmin)
+
+
+class QRCodeHistoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'link', 'scanned_at')
+    search_fields = ('user__username', 'link')
+admin.site.register(QRCodeHistory, QRCodeHistoryAdmin)
+
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'is_read', 'created_at')
+    search_fields = ('user__username', 'title')
+admin.site.register(Notification, NotificationAdmin)
