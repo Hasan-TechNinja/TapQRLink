d3bc58966a2bb8b0ec0127b5b1138ccd1ca5a959
Hasan-TechNinja <hasantechninja@gmail.com>
2025-09-04 17:28:31 +0600

    image bug solved

--- TapQRLink/urls.py
+++ TapQRLink/urls.py
@@ -33,4 +33,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
